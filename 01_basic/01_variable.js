5a31c692a756ad17dde35082087ab8868d453146
Rebelgoldenjha <162046755+Rebelgoldenjha@users.noreply.github.com>
2026-01-25 17:05:42 +0000

    data tpe

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountID = 3335
+let accountEmail = " rohit@gmail.com"
+accountCity = "jamshedpur"
+var accountpassword = "1233"
+let accountstate 
+
+// accountID = 2 // not change when you use const variable
+
+
+accountEmail  =  "fnjobb@gmail.com"
+accountCity= " sit"
+accountpassword =" 4444"
+console.log(accountID);
+
+/*  prefer not use var
+because of issue in block scope and function scope
+not asingend any value js print undefined
+*/
+console.table([accountEmail,accountpassword,accountCity])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,27 @@
+"use strick " // treat all js code as newe version
+
+// arert (3+3) // we aare using node js , not browser
+
+console.log(3+3)
+// code readebility should be high
+console.log("rohit")
+
+let = name ="Rohit"
+let age ="20"
+
+
+// number  => 2 to power 53
+// bigint 
+// string =>""
+// boolen => true / false 
+// null => standerd value
+// undefined =>
+    // symbol 
+
+
+// object 
+
+console.log(typeof age  ) // number
+console.log(typeof "rohit"  ) // string
+console.log(typeof null   ) // object 
+console.log(typeof undefined  ) // undefined 
