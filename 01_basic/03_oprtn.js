ff4706148433bf5b44d70f258648e66e5eac9aaf
Rebelgoldenjha <162046755+Rebelgoldenjha@users.noreply.github.com>
2026-01-25 17:18:54 +0000

    conversion

---
+++ 01_basic/03_oprtn.js
@@ -0,0 +1,22 @@
+let score = "7rvn" 
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+/*  
+output is
+string
+string
+number
+NaN 
+ if in the score value is null output is 0 
+ and undefined value is also undefined
+true = 1 
+false = 0
+vaise versa
+NaN is says not a number in value*/
+ 
