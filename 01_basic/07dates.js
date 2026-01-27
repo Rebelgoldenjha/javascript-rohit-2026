d26493fc369c39588e85e21af191311efe845461
ROHIT KUMAR JHA <162046755+Rebelgoldenjha@users.noreply.github.com>
2026-01-27 19:48:08 +0000

    date

---
+++ 01_basic/07dates.js
@@ -0,0 +1,19 @@
+// date 
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toUTCString())
+// console.log(myDate.getTimezoneOffset())
+
+let myCreateDate1 = new Date(2026, 1, 19)// 19 feb 2026
+let myCreateDate2 = new Date(2026, 1, 19 , 5 , 30, 9)// 19 feb 2026 5:30;09 AM
+ console.log(myCreateDate1.toLocaleDateString())
+ console.log(myCreateDate2.toLocaleDateString())
+
+ 
