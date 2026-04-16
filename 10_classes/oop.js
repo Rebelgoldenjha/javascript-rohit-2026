c0cff1b73fe125203728ba9b3edd5322f65be9f3
ROHIT KUMAR JHA <162046755+Rebelgoldenjha@users.noreply.github.com>
2026-04-16 14:26:38 +0000

    oop

---
+++ 10_classes/oop.js
@@ -0,0 +1,14 @@
+const user ={
+    username : "rohit",
+    logInCount: 8,
+    singIn : true,
+
+    getuserDetails: function(){
+        console.log(" got user details from database");
+
+    }
+}
+
+    console.log(user.username);
+    console.log(user.getuserDetails)
+
